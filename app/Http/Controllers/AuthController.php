5718b24a54835fd032346d1b3096cdf76381eb67
iman sadafi <sadafiiman@gmail.com>
2020-12-14 23:22:42 +0330

    + added function login and register and logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showRegister()
+    {
+        return view('auth.register');
+    }
+
+    public function register(RegisterRequest $request)
+    {
+
+       $user= User::create($request->validated());
+
+        if (Auth::attempt(['email' => $request->get('email'), 'password' => $request->get('password')]))
+            return redirect()->route('tasks.index')->withUser($user);//TODO: redirect to dashboard
+        else
+            return redirect()->back();//TODO: show message
+    }
+
+    public function showLogin()
+    {
+        return view('auth.login');
+    }
+    public function login(LoginRequest $request)
+    {
+        if (Auth::attempt(['email' => $request->get('email'), 'password' => $request->get('password')]))
+            return redirect()->route('tasks.index');//TODO: redirect to dashboard
+        else
+            return redirect()->back()->with('error','نام کاربری یا کلمه عبور صحیح نیست ! ');//TODO: show message
+    }
+
+    public function logout()
+    {
+        if(auth()->check()){
+            auth()->logout();
+            return redirect()->route('login');
+        }
+    }
+
+}
